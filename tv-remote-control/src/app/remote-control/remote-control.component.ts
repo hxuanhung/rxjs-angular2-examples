9db760f92fcd5a0c4f683e5ca1ce1ca32f15a4f7
hxuanhung <hxuanhung@gmail.com>
2017-01-27 09:51:08 +0100

    chore(tv-remote-control): unsubscribe on reset and onDestroy

--- tv-remote-control/src/app/remote-control/remote-control.component.ts
+++ tv-remote-control/src/app/remote-control/remote-control.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Rx';
 const TIME = 2000;
 @Component({
@@ -7,7 +8,7 @@ const TIME = 2000;
   templateUrl: './remote-control.component.html',
   styleUrls: ['./remote-control.component.css']
 })
-export class RemoteControlComponent implements OnInit {
+export class RemoteControlComponent implements OnInit, OnDestroy {
   @Input() maxInput: number = 0;
   numberSbj = new Subject();
 
@@ -16,18 +17,25 @@ export class RemoteControlComponent implements OnInit {
     .distinctUntilChanged()
   progress$: Observable<string>;
   progress: string;
+  progressSub: Subscription;
   constructor() { }
 
   ngOnInit() {
     this.init();
   }
 
+  ngOnDestroy() {
+    this.progressSub.unsubscribe();
+  }
+
   init() {
     this.progress$ = this.number$.takeUntil(Observable.timer(TIME * this.maxInput)).repeat();
-    this.progress$.subscribe(x => this.progress = x);
+    this.progressSub = this.progress$.subscribe(x => {
+      this.progress = x});
   }
 
   resetInput() {
+    this.progressSub.unsubscribe();
     this.progress$ = Observable.empty();
     this.init();
   }
